c564de848d861576f2e619885d66199a0a604e8b
JinxitNow <justinekayser@gmail.com>
2025-02-10 09:21:47 +0100

    Added jquery

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#header").load("header.html");
+    $("#content").load("why-planner.html"); // Default content
+    $("#footer").load("footer.html");
+
+    // Example: Load testimonials when a button is clicked
+    $(".cta-button").on("click", function(event) {
+        event.preventDefault();
+        $("#content").load("testimonials.html");
+    });
+});
+
