f0a827c13358fb0ae82bf8a48ef4f4916c4cf635
JinxitNow <justinekayser@gmail.com>
2025-02-11 23:13:40 +0100

    made some more rough content

--- script.js
+++ script.js
@@ -10,11 +10,14 @@ $(document).ready(function() {
     });
 });
 
-$(".dropdown").hover(
-    function () {
-        $(this).find(".dropdown-menu").stop(true, true).slideDown(200);
-    },
-    function () {
-        $(this).find(".dropdown-menu").stop(true, true).slideUp(200);
-    }
-);
+$(document).ready(function () {
+    $(".dropdown").hover(
+        function () {
+            $(this).find(".dropdown-menu").stop(true, true).slideDown(200);
+        },
+        function () {
+            $(this).find(".dropdown-menu").stop(true, true).slideUp(200);
+        }
+    );
+});
+
