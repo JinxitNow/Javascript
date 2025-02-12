a58a46244977263d5459c0b614244a615a4325bd
helenalenskjold <lenskjold2000@gmail.com>
2025-02-12 11:26:32 +0100

    opdateret script

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ $(document).ready(function() {
 $( "#header" ).load( "header.html", function() {
     $.getScript("header.js")
   });
-    $("#hero").load("hero.html"); 
-    $("#products").load("products.html"); 
-    $("#slideshow").load("slideshow.html"); 
-    $("#pop-up").load("pop-up.html"); 
-    $("#footer").load("footer.html");
+  $( "#hero" ).load( "hero.html", function() {
+    $.getScript("hero.js")
+  });
+  $( "#products" ).load( "products.html", function() {
+    $.getScript("products.js")
+  });
+  $( "#slideshow" ).load( "slideshow.html", function() {
+    $.getScript("slideshow.js")
+  });
+  $( "#pop-up" ).load( "pop-up.html", function() {
+    $.getScript("pop-up.js")
+  });
+  $( "#footer" ).load( "footer.html", function() {
+    $.getScript("footer.js")
+  });
 });
 
