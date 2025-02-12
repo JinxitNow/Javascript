5b607fb8e8aee9c8984b6f23cca79e1100834ba9
helenalenskjold <lenskjold2000@gmail.com>
2025-02-12 12:22:31 +0100

    button er synligt

--- footer.js
+++ footer.js
@@ -1,62 +1,49 @@
-
-    let script = document.createElement("script");
-    script.onload = function() {
-   
-        $(document).ready(function() {
-            let backToTop = {
-
-
-                
-                button: $("#backToTopBtn"),
-                scrollThreshold: 100, // Når man scroller 100px, vises knappen
-                steps: 20, // Antal trin i smooth scroll-effekten, gør at den kører langsomt op (i 20 trins)
-                alertMessages: ["Back to the top!", "See you at the top!", "Back to the top we go!"], // Tilfældige beskeder
-            
-                
-                init: function () {
-                    let self = this;
-            
-                    $(window).on("scroll", function() {
-                        if ($(document).scrollTop() > self.scrollThreshold) {
-                            self.button.show();
-                        } else {
-                            self.button.hide();
-                        }
-                    });
-            
-                    this.button.on("click", function() {
-                        let messageIndex = Math.floor(Math.random() * self.alertMessages.length);
-                        alert(self.alertMessages[messageIndex]);
-                        self.smoothScrollToTop();
-                    });
-                },
-            
-                smoothScrollToTop: function () {
-                    let position = $(document).scrollTop();
-                    let step = position / this.steps;
-            
-                    let interval = setInterval(() => {
-                        if (position > 0) {
-                            position -= step;
-                            $("html, body").scrollTop(position);
-                        } else {
-                            clearInterval(interval);
-                        }
-                    }, 10);
+$(document).ready(function() {
+    let backToTop = {
+        button: $("#backToTopBtn"),
+        scrollThreshold: 100, // Når man scroller 100px, vises knappen
+        steps: 20, // Antal trin i smooth scroll-effekten
+        alertMessages: ["Back to the top!", "See you at the top!", "Back to the top we go!"], // Tilfældige beskeder
+    
+        init: function () {
+            let self = this;
+    
+            $(window).on("scroll", function() {
+                if ($(document).scrollTop() > self.scrollThreshold) {
+                    self.button.show();
+                } else {
+                    self.button.hide();
                 }
-            };
-            
-            // Start objektet
-            backToTop.init();
-        });
+            });
+    
+            this.button.on("click", function() {
+                let messageIndex = Math.floor(Math.random() * self.alertMessages.length);
+                alert(self.alertMessages[messageIndex]);
+                self.smoothScrollToTop();
+            });
+        },
+    
+        smoothScrollToTop: function () {
+            let position = $(document).scrollTop();
+            let step = position / this.steps;
+    
+            let interval = setInterval(() => {
+                if (position > 0) {
+                    position -= step;
+                    $("html, body").scrollTop(position);
+                } else {
+                    clearInterval(interval);
+                }
+            }, 10);
+        }
     };
 
-    // drop down i footeren
-    $(document).ready(function() {
-        $("#valuta").change(function() {
-            var selected = $("#valuta option:selected").text();
-            $("#selectedCurrency").text("Valgt valuta: " + selected);
-        });
-    });
-    document.head.appendChild(script);
+    // Start objektet
+    backToTop.init();
 
+    // Drop down i footeren
+    $("#valuta").change(function() {
+        var selected = $("#valuta option:selected").text();
+        $("#selectedCurrency").text("Valgt valuta: " + selected);
+    });
+});
