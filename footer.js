8120eebb5897f3131bbd09abab397c9bfe9f6b75
helenalenskjold <lenskjold2000@gmail.com>
2025-02-13 12:55:20 +0100

    console.log

--- footer.js
+++ footer.js
@@ -6,16 +6,24 @@ $(document).ready(function() {
         alertMessages: ["Back to the top!", "See you at the top!", "Back to the top we go!"], // Tilfældige beskeder
     
         init() {
+            console.log("Initializing Back to Top button...");
+            
             $(window).on("scroll", () => {
-                if ($(document).scrollTop() > this.scrollThreshold) {
+                const scrollPosition = $(document).scrollTop();
+                console.log("Scroll position:", scrollPosition);
+                
+                if (scrollPosition > this.scrollThreshold) {
                     this.button.show();
+                    console.log("Button is visible");
                 } else {
                     this.button.hide();
+                    console.log("Button is hidden");
                 }
             });
 
             this.button.on("click", () => {
                 const messageIndex = Math.floor(Math.random() * this.alertMessages.length);
+                console.log("Random message index:", messageIndex);
                 alert(this.alertMessages[messageIndex]);
                 this.smoothScrollToTop();
             });
@@ -24,13 +32,16 @@ $(document).ready(function() {
         smoothScrollToTop() {
             let position = $(document).scrollTop();
             const step = position / this.steps; 
+            console.log("Starting smooth scroll, initial position:", position, "step size:", step);
     
             let interval = setInterval(() => {
                 if (position > 0) {
                     position -= step;
                     $("html, body").scrollTop(position);
+                    console.log("Scrolling, current position:", position);
                 } else {
                     clearInterval(interval);
+                    console.log("Smooth scroll complete.");
                 }
             }, 10);
         }
@@ -42,6 +53,7 @@ $(document).ready(function() {
     // Drop down i footeren
     $("#valuta").change(() => {
         const selected = $("#valuta option:selected").text();
+        console.log("Selected currency:", selected);
         $("#selectedCurrency").text("Valgt valuta: " + selected);
     });
 });
